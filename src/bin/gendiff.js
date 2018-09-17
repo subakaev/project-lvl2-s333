6a8e59c54c4da563b2bd4e0075e23434d95f7140
Sergey Bakaev <bakaev@yandex.ru>
2018-09-17 21:15:18 +0300

    configure commander

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,5 +5,7 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f', 'Format')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .action(() => console.log('test message from commander'))
   .parse(process.argv);
