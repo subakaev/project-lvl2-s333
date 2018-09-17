6435ab5573dee52d149b9421c1f0fe72b9cfd713
Sergey Bakaev <bakaev@yandex.ru>
2018-09-17 21:46:57 +0300

    solving problem with global installation of published package

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@
 import program from 'commander';
 
 program
-  .version('0.0.1')
+  .version('0.0.4')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
