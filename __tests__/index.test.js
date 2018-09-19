4ba0b17d0cf07c9e49f29f6b9db0a248b5c86157
Sergey Bakaev <bakaev@yandex.ru>
2018-09-19 19:25:46 +0300

    new tests for tree structures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -22,3 +22,52 @@ test('test gendiff flat yml', () => {
 test('test gendiff flat ini', () => {
   expect(gendiff(`${flatPath}/before.ini`, `${flatPath}/after.ini`)).toBe(expectedResult);
 });
+
+const expectedTreeResult = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: {
+          key: value
+      }
+      setting6: {
+          key: value
+        + ops: vops
+      }
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+  }
+  group1: {
+    + baz: bars
+    - baz: bas
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+  }
++ group3: {
+      fee: 100500
+  }
+}`;
+
+const treePath = '__tests__/__fixtures__/tree/';
+
+test('test gendiff tree json', () => {
+  expect(gendiff(`${treePath}/before.json`, `${treePath}/after.json`)).toBe(expectedTreeResult);
+});
+
+test('test gendiff tree yml', () => {
+  expect(gendiff(`${treePath}/before.yml`, `${treePath}/after.yml`)).toBe(expectedTreeResult);
+});
+
+test('test gendiff tree ini', () => {
+  expect(gendiff(`${treePath}/before.ini`, `${treePath}/after.ini`)).toBe(expectedTreeResult);
+});
