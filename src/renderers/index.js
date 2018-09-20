03603751cb98081622bb48f7b6f1e65dcf8e5a9c
Sergey Bakaev <bakaev@yandex.ru>
2018-09-20 22:26:02 +0300

    json renderer implementation

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,10 +1,12 @@
 import renderAstAsTree from './tree';
 import renderAstAsPlain from './plain';
+import renderAstAsJSON from './json';
 
 export default (renderFormat) => {
   const renderers = {
     tree: renderAstAsTree,
     plain: renderAstAsPlain,
+    json: renderAstAsJSON,
   };
 
   return renderers[renderFormat] || renderers.tree;
