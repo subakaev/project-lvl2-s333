07aa10258b16b41e540e18368eac48ce90bb46e7
Sergey Bakaev <bakaev@yandex.ru>
2018-09-20 21:42:04 +0300

    refactoring renderer according to new test data

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -51,6 +51,18 @@ const renderAstAsTree = (ast, depth = 1) => {
   return `{\n${_.flatten(arr).join('\n')}\n${getIndentForCloseBrace(depth)}}`;
 };
 
+const renderPlainValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+
+  return value;
+};
+
 const renderAstAsPlain = (ast, prefix = '') => {
   const rows = ast.map((node) => {
     const {
@@ -59,17 +71,20 @@ const renderAstAsPlain = (ast, prefix = '') => {
 
     const propertyName = prefix !== '' ? `${prefix}.${name}` : name;
 
+    const value1Str = renderPlainValue(value1);
+    const value2Str = renderPlainValue(value2);
+
     switch (type) {
       case 'node':
         return renderAstAsPlain(children, `${propertyName}`);
       case 'unchanged':
         return null;
       case 'added':
-        return `Property '${propertyName}' was added with value: ${value2}`;
+        return `Property '${propertyName}' was added with value: ${value2Str}`;
       case 'deleted':
         return `Property '${propertyName}' was removed`;
       case 'diff':
-        return `Property '${propertyName}' was updated. From ${value1} to ${value2}`;
+        return `Property '${propertyName}' was updated. From ${value1Str} to ${value2Str}`;
       default:
         throw new Error();
     }
