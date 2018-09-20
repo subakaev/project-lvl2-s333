017e9bd196cb76f292dbce3e4c31961ae6d0fe2c
Sergey Bakaev <bakaev@yandex.ru>
2018-09-20 22:03:44 +0300

    update version number

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import gendiff from '..';
 
 program
-  .version('0.0.8')
+  .version('0.0.9')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
