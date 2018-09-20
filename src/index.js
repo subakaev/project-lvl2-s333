32fff6b8b5968034584dcab4aeb928461455ad9c
Sergey Bakaev <bakaev@yandex.ru>
2018-09-20 19:14:01 +0300

    remove old ast version

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import renderAst from './renderers';
 
 const getConfigKeys = (config1, config2) => _.union(_.keys(config1), _.keys(config2));
 
-const generateAst2 = (obj1, obj2) => getConfigKeys(obj1, obj2).map((key) => {
+const generateAst = (obj1, obj2) => getConfigKeys(obj1, obj2).map((key) => {
   const value1 = obj1[key];
   const value2 = obj2[key];
 
@@ -20,7 +20,7 @@ const generateAst2 = (obj1, obj2) => getConfigKeys(obj1, obj2).map((key) => {
   }
 
   if (_.isObject(value1) && _.isObject(value2)) {
-    return { name: key, type: 'node', children: generateAst2(value1, value2) };
+    return { name: key, type: 'node', children: generateAst(value1, value2) };
   }
 
   if (value1 === value2) {
@@ -32,30 +32,6 @@ const generateAst2 = (obj1, obj2) => getConfigKeys(obj1, obj2).map((key) => {
   };
 });
 
-const generateAst = (obj1, obj2) => getConfigKeys(obj1, obj2).reduce((acc, key) => {
-  const value1 = obj1[key];
-  const value2 = obj2[key];
-
-  if (!_.has(obj2, key)) {
-    return { ...acc, [key]: { type: 'deleted', value1 } };
-  }
-
-  if (!_.has(obj1, key)) {
-    return { ...acc, [key]: { type: 'added', value2 } };
-  }
-
-  if (_.isObject(value1) && _.isObject(value2)) {
-    return { ...acc, [key]: { type: 'node', children: generateAst(value1, value2) } };
-  }
-
-  if (value1 === value2) {
-    return { ...acc, [key]: { type: 'unchanged', value1 } };
-  }
-
-  return { ...acc, [key]: { type: 'both', value1, value2 } };
-}, {});
-
-
 export default (configFile1, configFile2) => {
   const content1 = fs.readFileSync(configFile1, 'utf8');
   const content2 = fs.readFileSync(configFile2, 'utf8');
@@ -65,7 +41,7 @@ export default (configFile1, configFile2) => {
   const config1 = parseContent(extName, content1);
   const config2 = parseContent(extName, content2);
 
-  const ast = generateAst2(config1, config2);
+  const ast = generateAst(config1, config2);
 
   return renderAst(ast);
 };
--- src/renderers/index.js
+++ src/renderers/index.js
@@ -16,7 +16,7 @@ const renderValue = (value, tabsCount = 1) => {
 
 const keyValueToString = (key, value, tabsCount) => `${key}: ${renderValue(value, tabsCount)}`;
 
-const renderAst2 = (ast, depth = 1) => {
+const renderAst = (ast, depth = 1) => {
   const indent = tabChar.repeat(depth);
 
   const arr = ast.map((node) => {
@@ -26,7 +26,7 @@ const renderAst2 = (ast, depth = 1) => {
 
     switch (type) {
       case 'node':
-        return `${indent}  ${name}: ${renderAst2(children, depth + 2)}`;
+        return `${indent}  ${name}: ${renderAst(children, depth + 2)}`;
       case 'unchanged':
         return `${indent}  ${keyValueToString(name, value1, depth + 2)}`;
       case 'added':
@@ -46,32 +46,4 @@ const renderAst2 = (ast, depth = 1) => {
   return `{\n${_.flatten(arr).join('\n')}\n${tabChar.repeat(depth - 1)}}`;
 };
 
-const renderAst = (ast, tabsCount = 1) => {
-  const arr = _.keys(ast).map((key) => {
-    const node = ast[key];
-
-    const indent = tabChar.repeat(tabsCount);
-
-    switch (node.type) {
-      case 'node':
-        return `${indent}  ${key}: ${renderAst(node.children, tabsCount + 2)}`;
-      case 'unchanged':
-        return `${indent}  ${keyValueToString(key, node.value1, tabsCount + 2)}`;
-      case 'added':
-        return `${indent}+ ${keyValueToString(key, node.value2, tabsCount + 1)}`;
-      case 'deleted':
-        return `${indent}- ${keyValueToString(key, node.value1, tabsCount + 1)}`;
-      case 'both':
-        return [
-          `${indent}+ ${keyValueToString(key, node.value2, tabsCount + 1)}`,
-          `${indent}- ${keyValueToString(key, node.value1, tabsCount + 1)}`,
-        ];
-      default:
-        throw new Error();
-    }
-  });
-
-  return `{\n${_.flatten(arr).join('\n')}\n${tabChar.repeat(tabsCount - 1)}}`;
-};
-
-export default renderAst2;
+export default renderAst;
