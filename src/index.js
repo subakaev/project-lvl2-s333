0f5551ce993575da6c3c338cbd594471141d0758
Sergey Bakaev <sergey.bakaev@hotmail.com>
2018-09-20 16:38:17 +0300

    second variant of ast - array of nodes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,31 @@ import renderAst from './renderers';
 
 const getConfigKeys = (config1, config2) => _.union(_.keys(config1), _.keys(config2));
 
+const generateAst2 = (obj1, obj2) => getConfigKeys(obj1, obj2).map((key) => {
+  const value1 = obj1[key];
+  const value2 = obj2[key];
+
+  if (!_.has(obj2, key)) {
+    return { name: key, type: 'deleted', value1 };
+  }
+
+  if (!_.has(obj1, key)) {
+    return { name: key, type: 'added', value2 };
+  }
+
+  if (_.isObject(value1) && _.isObject(value2)) {
+    return { name: key, type: 'node', children: generateAst2(value1, value2) };
+  }
+
+  if (value1 === value2) {
+    return { name: key, type: 'unchanged', value1 };
+  }
+
+  return {
+    name: key, type: 'diff', value1, value2,
+  };
+});
+
 const generateAst = (obj1, obj2) => getConfigKeys(obj1, obj2).reduce((acc, key) => {
   const value1 = obj1[key];
   const value2 = obj2[key];
@@ -40,7 +65,7 @@ export default (configFile1, configFile2) => {
   const config1 = parseContent(extName, content1);
   const config2 = parseContent(extName, content2);
 
-  const ast = generateAst(config1, config2);
+  const ast = generateAst2(config1, config2);
 
   return renderAst(ast);
 };
--- src/renderers/index.js
+++ src/renderers/index.js
@@ -16,6 +16,36 @@ const renderValue = (value, tabsCount = 1) => {
 
 const keyValueToString = (key, value, tabsCount) => `${key}: ${renderValue(value, tabsCount)}`;
 
+const renderAst2 = (ast, depth = 1) => {
+  const indent = tabChar.repeat(depth);
+
+  const arr = ast.map((node) => {
+    const {
+      name, type, value1, value2, children,
+    } = node;
+
+    switch (type) {
+      case 'node':
+        return `${indent}  ${name}: ${renderAst2(children, depth + 2)}`;
+      case 'unchanged':
+        return `${indent}  ${keyValueToString(name, value1, depth + 2)}`;
+      case 'added':
+        return `${indent}+ ${keyValueToString(name, value2, depth + 1)}`;
+      case 'deleted':
+        return `${indent}- ${keyValueToString(name, value1, depth + 1)}`;
+      case 'diff':
+        return [
+          `${indent}+ ${keyValueToString(name, value2, depth + 1)}`,
+          `${indent}- ${keyValueToString(name, value1, depth + 1)}`,
+        ];
+      default:
+        throw new Error();
+    }
+  });
+
+  return `{\n${_.flatten(arr).join('\n')}\n${tabChar.repeat(depth - 1)}}`;
+};
+
 const renderAst = (ast, tabsCount = 1) => {
   const arr = _.keys(ast).map((key) => {
     const node = ast[key];
@@ -44,4 +74,4 @@ const renderAst = (ast, tabsCount = 1) => {
   return `{\n${_.flatten(arr).join('\n')}\n${tabChar.repeat(tabsCount - 1)}}`;
 };
 
-export default renderAst;
+export default renderAst2;
